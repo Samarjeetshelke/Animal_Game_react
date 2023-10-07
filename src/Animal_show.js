6da4f91c9c4c71bd089bd96ed77856efd28d2fbe
samarjeet shelke <59022780+Samarjeetshelke@users.noreply.github.com>
2023-10-07 18:00:09 +0000

    Update 4 files

---
+++ src/Animal_show.js
@@ -0,0 +1,5 @@
+function Animal_show(prop) {
+  return <h1>{prop.type}</h1>;
+}
+
+export default Animal_show;
--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import "./styles.css";
+import { useState } from "react";
+import Animal_show from "./Animal_show";
 
 export default function App() {
+  const [animals, setAnimals] = useState([]); //array destructuring
+
+  const handelx = () => {
+    setAnimals([...animals, getRandomAnimal()]);
+  };
+
+  function getRandomAnimal() {
+    const ani = ["dog", "cat", "horse", "bird", "Crow"];
+
+    return ani[Math.floor(Math.random() * ani.length)];
+  }
+
+  const randerAni = animals.map((animal) => {
+    return <Animal_show type={animal} />;
+  });
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="App" style={{ float: "inline-block" }}>
+      <button onClick={handelx}>Add Animal</button>
+      <p>{randerAni}</p>
     </div>
   );
 }
